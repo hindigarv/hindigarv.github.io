c71fa3b65f52e11366154969b8d1692c12289db5
hindigarv <hindi.garv.001@gmail.com>
2022-06-16 22:35:36 +0200

    remove invalid attribute to set height

--- hindigarv/src/TextPad/Result/Result.js
+++ hindigarv/src/TextPad/Result/Result.js
@@ -24,7 +24,6 @@ function Result(props) {
             columns={columns}
             autoHeight
             disableSelectionOnClick
-            etRowHeight={() => "auto"}
           />
         </div>
       </div>
