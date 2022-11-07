820d4d9a94ed7bb892be469e07de3cab7582d322
hindigarv <hindi.garv.001@gmail.com>
2022-11-07 23:18:57 +0100

    create a main component

--- hindigarv/src/App.js
+++ hindigarv/src/App.js
@@ -1,25 +1,8 @@
 import './App.css';
+import Main from "./components/Main";
 
 function App() {
-  return (
-      <div className="main roundedCorner">
-        <textarea className="editor roundedCorner">
-            {
-`हिन्दी गर्व में आप का स्वागत है।
-क्या आप को जानना है कि आप की हिन्दी में कोई विदेशी भाषा के शब्द है कि नहीं?
-तो इस विभाग में अपना वाक्य लिखे।
-
-उदाहरण: 
-निम्न लिखित वाक्य में ३ विदेशी शब्द है।
-"में दस साल से इंतज़ार कर रहा था इस टाइम का।"
-
-उपरोक्त वाक्य में "साल" एक फ़ारसी शब्द है
-TODO
-`
-            }
-</textarea>
-      </div>
-  );
+  return <Main />;
 }
 
 export default App;
