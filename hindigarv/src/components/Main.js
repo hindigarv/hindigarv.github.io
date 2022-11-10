0d08fb5f4900ccc3cb62c25562f97e2a2a57c4b3
hindigarv <hindi.garv.001@gmail.com>
2022-11-11 00:11:35 +0100

    updated links

--- hindigarv/src/components/Main.js
+++ hindigarv/src/components/Main.js
@@ -16,7 +16,7 @@ const Main = function () {
 `;
 
     const [text, setText] = useState(initialText)
-    const [result, setResult] = useState([])
+    const [result, setResult] = useState(find(text))
 
     const handleChange = function (event) {
         setResult(find(event.target.value));
@@ -29,24 +29,25 @@ const Main = function () {
                     <textarea className="editor" onChange={handleChange} defaultValue={text}></textarea>
                 </div>
                 <div className="out">
-                    <ul>
+                    {result && <ul>
                         {result.map(word => <li key={word.shabda}>{word.shabda} ({word.mool}) -> {word.paryaya}</li>)}
                     </ul>
+                    }
                 </div>
             </main>
             <div className="side">
                 <div className="icon">
-                    <a href="http://www.t.co/1123">
+                    <a href="https://twitter.com/HindiGarv" target="_blank" rel="noopener noreferrer">
                         <img src="./img/icons8-twitter-48.png" height="48" width="48" />
                     </a>
                 </div>
                 <div className="icon">
-                    <a href="http://www.google.com/123">
+                    <a href="https://chrome.google.com/webstore/detail/%E0%A4%B9%E0%A4%BF%E0%A4%A8%E0%A5%8D%E0%A4%A6%E0%A5%80-%E0%A4%97%E0%A4%B0%E0%A5%8D%E0%A4%B5/annghchohpbikcnhhdebobkcmmjfolih" target="_blank" rel="noopener noreferrer">
                         <img src="./img/icons8-chrome-48.png" alt="Install Chrome Extension" height="48" width="48" />
                     </a>
                 </div>
                 <div className="icon">
-                    <a href="http://www.google.com/123">
+                    <a href="https://t.me/HindiGarvBot" target="_blank" rel="noopener noreferrer">
                         <img src="./img/icons8-telegram-app-48.png" alt="Checkout the Telegram bot"
                              height="48" width="48" />
                     </a>
